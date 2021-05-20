470a537deb5c946c073602ac7b95700b36f98bf7
Klinker195 <gianlucaviscardi@gmail.com>
2021-05-20 10:45:18 +0200

    Project file update and main includes
    Upload of updated .dev and .layout files. Addition of includes in main.
    
    Co-Authored-By: Random_Leviathan <74977887+Random-Leviathan890@users.noreply.github.com>
    Co-Authored-By: Kranidos1 <76439725+Kranidos1@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+#include "headers/cliui.h"
+#include "headers/utils.h"
+#include "headers/datastructures.h"
 
 int main(void) {
 	
